399d1f0d868419de53abd3c305ab439fee13dbe9
Miguel Mateo <ing.miguelmateotavarez@gmail.com>
2024-10-08 08:03:56 -0400

    feat: auth controller and routes created

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { Server } from "./presentation/server";
+import { envs } from './config/envs';
+import { AppRoutes } from "./presentation/appRoutes";
 
 async function main(){
     //todo: await database
-    //todo: server startup
     new Server({
-        
+        port: envs.PORT,
+        routes: AppRoutes.routes
     }).start();
 }
 
